63f2d3f051cef1f10c5971994506abb99afc1a04
James White <cyberyt@gmail.com>
2015-10-18 22:33:48 +0100

    FE: Started work on other post bit on FE, but back end and seeds need more work

--- front-end/js/controller.js
+++ front-end/js/controller.js
@@ -1,16 +1,16 @@
 angular.module('likeAngularApp')
        .controller('PhatController', PhatController);
 
-       PhatController.$inject = ['$resource']
+       PhatController.$inject = ['$resource', 'angularMoment']
 
-function PhatController ($resource) {
+function PhatController ($resource, angularMoment) {
   var self = this;
   var Post = $resource('http://localhost:3000/posts/:id', { id: '@_id'})
   // may need a custom put request later on.
 
  this.posts = Post.query();
 
- 
+
 
 
 
